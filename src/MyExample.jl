4dc18929ac464972f35892ccd0b4c0db4e13a6e4
Andries <j.andries.j.steenkamp@gmail.com>
2020-10-21 08:48:43 +0200

    add the my_f linear function used for demonstration.

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -2,4 +2,10 @@ module MyExample
 
 # Write your package code here.
 
+greet() = print("Hello world!")
+
+include("extra_file.jl")
+
+export my_f
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
+#  You have to activate the enviroment in the folder
+# suppressed export way
+import MyExample
+MyExample.my_f(2,1)
+# all exports allowed
 using MyExample
 using Test
 
+
+
+
 @testset "MyExample.jl" begin
-    # Write your tests here.
+    @test my_f(2,1) == 5
+    @test my_f(1,5) == 7
+
 end
